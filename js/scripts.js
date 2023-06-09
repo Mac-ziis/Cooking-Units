6b38f3b1acbecb4413519ff3bb61016ca98066c5
Mac-ziis <macg286@gmail.com>
2023-06-08 18:23:51 -0700

    Add the first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function ozToGrams(number1) {
+  return number1 * 28.4;
+}
+
+const number1 = parseInt(prompt("enter a number:"))
+
+window.alert("That will equal: " + ozToGrams(number1) + " grams.");
+
+//1oz to 28.4 grams
